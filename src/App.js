13d74eb96deb7ea4961682fa13fa76cf52a5113e
Ashley <ashleythadickal@gmail.com>
2022-01-17 09:41:36 -0500

    API data receive; Next: get data on page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
+  const [nasaItem, setNasaItem] = useState([])
+
+  useEffect(() => {
+    axios({
+      url: 'https://images-api.nasa.gov/search',
+      method: 'GET',
+      dataResponse: 'json',
+      params: {
+        q: 'moon',
+        media_type: 'image'
+      },
+    }).then((response) => {
+      // *********************** is the below the proper way to return a shorter response if the API does not provide for an option to limit the response? Is this the proper way to handle this 'error in spelling?'
+      const shorterResponse = (response.data.collection.items).slice(0, 9)
+      setNasaItem(shorterResponse);
+    })
+  }, [])
+  console.log(nasaItem)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
