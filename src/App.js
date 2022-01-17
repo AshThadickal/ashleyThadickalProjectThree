d581249e67c4dd6a0fa6016dec8940ed4d8bf1da
Ashley <ashleythadickal@gmail.com>
2022-01-17 13:41:41 -0500

    User Input Passed to API Query; Next: Handle Spelling Error

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css';
 import DisplayResults from './DisplayResults.js';
 
+
 function App() {
   const [nasaItem, setNasaItem] = useState([])
+  const [userInput, setUserInput] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
 
-  useEffect(() => {
+  const handleInput = (event) => {
+    setUserInput(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearchTerm(userInput);
     axios({
       url: 'https://images-api.nasa.gov/search',
       method: 'GET',
       dataResponse: 'json',
       params: {
-        q: 'moon',
+        q: searchTerm,
         media_type: 'image'
       },
     }).then((response) => {
-      // *********************** is the below the proper way to return a shorter response if the API does not provide for an option to limit the response? Is this the proper way to handle this 'error in spelling?'
       const shorterResponse = (response.data.collection.items).slice(0, 9)
       setNasaItem(shorterResponse);
     })
-  }, [])
-  console.log(nasaItem)
+
+  }
+
+
 
   return (
     <div className="App">
       <h1 id='home'>A look at Space: <span>Images from NASA</span></h1>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="search">Search for Space:</label>
+        <input type="text" id="search" onChange={handleInput} value={userInput} />
+        <button>Search</button>
+      </form>
 
       {
         nasaItem.map((nasaObject) => {
